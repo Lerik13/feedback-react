f064d38271a14819ad05b280ca527f1febfaa358
Valeria <lerik13@gmail.com>
2022-03-21 22:39:07 -0600

    FeedbackForm component for adding new review with rating

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+import RatingSelect from "./RatingSelect"
+import Button from "./shared/Button"
+import Card from "./shared/Card"
+
+function FeedbackForm({handleAdd}) {
+	const [text, setText] = useState('')
+	const [rating, setRating] = useState(10)
+	const [btnDisabled, setBtnDisabled] = useState(true)
+	const [message, setMessage] = useState('')
+	
+	const handleTextChange = (e) => {
+		const new_text = e.target.value
+		if (new_text === '') {
+			setBtnDisabled(true)
+			setMessage(null)
+		} else if(new_text !== '' && new_text.trim().length <= 10) {
+			setMessage('Text must be at least 10 characters')
+			setBtnDisabled(true)
+		} else {
+			setMessage(null)
+			setBtnDisabled(false)
+		}
+		
+		setText(new_text)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if (text.trim().length > 10) {
+			const newFeedback = {
+				text,
+				rating
+			}
+			handleAdd(newFeedback)
+			setText('')
+		}
+	}
+
+	return (
+		<Card>
+			<form onSubmit={handleSubmit}>
+				<h2>How would you rate your service with us?</h2>
+				<RatingSelect select={(rating) => setRating(rating)} />
+				<div className="input-group">
+					<input
+						value={text}
+						onChange={handleTextChange}
+						type="text"
+						placeholder='Write a review' 
+					/>
+					<Button type="submit" isDisabled={btnDisabled}>Send</Button>
+				</div>
+
+				{message && <div className="message">{message}</div>}
+			</form>
+		</Card>
+	)
+}
+
+export default FeedbackForm
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
-import PropTypes from 'prop-types';
+import {motion, AnimatePresence} from 'framer-motion'
+import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 
 function FeedbackList({ feedback, handleDelete }) {
@@ -6,6 +7,26 @@ function FeedbackList({ feedback, handleDelete }) {
 		return <p>No Feedback yet</p>
 	}
 	return (
+		<div className="feedback-list">
+			<AnimatePresence>
+				{feedback.map((item) => (
+					<motion.div 
+						key={item.id}
+						initial={{opacity: 0}}
+						animate={{opacity: 1}}
+						exit={{opacity: 0}}
+					>
+						<FeedbackItem 
+							key={item.id}
+							item={item}
+							handleDelete={handleDelete}
+						/>
+					</motion.div>
+				))}
+			</AnimatePresence>
+		</div>
+	)
+/* 	return (
 		<div className="feedback-list">
 			{feedback.map((item) => (
 				<FeedbackItem 
@@ -15,13 +36,13 @@ function FeedbackList({ feedback, handleDelete }) {
 				/>
 			))}
 		</div>
-	)
+	) */
 }
 
 FeedbackList.propTypes = {
 	feedback: PropTypes.arrayOf(
 		PropTypes.shape({
-			id: PropTypes.number.isRequired,
+			id: PropTypes.string.isRequired,
 			text: PropTypes.string.isRequired,
 			rating: PropTypes.number.isRequired,
 
